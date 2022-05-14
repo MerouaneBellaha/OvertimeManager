7bbe6634027c5aea07cf505bba9b406c7b0a4915
Merouane <bellaha.merouane@gmail.com>
2022-05-14 09:37:21 +0200

    init

---
+++ Shared/OvertimeManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OvertimeManagerApp.swift
+//  Shared
+//
+//  Created by Merouane Bellaha on 14/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct OvertimeManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
